e78c94d05442d39eaa424522501e09214c565200
depaulo <adelsondepaulo@hotmail.com>
2019-09-29 22:31:20 -0300

    bootstrap added

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Job
+from .models import Job #Need to add to see Job on the website
 
 # Register your models here.
-admin.site.register(Job)
+admin.site.register(Job) #complement of the import job above
--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Job #to let home get the jobs
 
 # Create your views here.
 def home(request):
-    return render(request, 'jobs/home.html')#added this lines of code because of the new path created in urls
+    jobs = Job.objects #passing jobs to home
+    return render(request, 'jobs/home.html',{'jobs':jobs})#added this lines of code because of the new path created in urls
+
