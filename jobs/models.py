1e4b1fcdcb7d3adfab942466eaa2ceb53fea2180
depaulo <adelsondepaulo@hotmail.com>
2019-10-04 22:41:14 -0300

    version 2.0

--- jobs/models.py
+++ jobs/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Job(models.Model):    
     image = models.ImageField(upload_to='images/') #images, are upload here 
     summary = models.CharField(max_length=200) #summary
+    description = models.TextField(blank=True) #description
 
-    def __str__(self):# this is necessary to show the description on the admin module
+    def __str__(self):# this is necessary to show the summary on the admin module
         return self.summary
